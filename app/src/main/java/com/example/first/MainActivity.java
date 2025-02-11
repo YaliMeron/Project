1d49748127746fe2894beb948c0216460ffa6b67
YaliMerom <yalimeron10@gmail.com>
2025-02-11 21:42:51 +0200

    with java

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -1,70 +1,112 @@
 package com.example.first;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView inputTextView;
-    private StringBuilder currentInput = new StringBuilder();
-    private static final int MAX_WORD_LENGTH = 5;
+    private final String ANSWER = "WATER";
+    private StringBuilder currentGuess = new StringBuilder();
+    private int row = 0;
+    private TextView[][] grid = new TextView[6][5];
+    private Button[] keyboardButtons = new Button[26];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Make sure your XML file matches this
+        setContentView(R.layout.activity_main);
 
-        inputTextView = findViewById(R.id.wordInput);
-        setupKeyboard();
+        initializeGrid();
+        initializeKeyboard();
     }
 
-    private void setupKeyboard() {
-        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private void initializeGrid() {
+        for (int r = 0; r < 6; r++) {
+            for (int c = 0; c < 5; c++) {
+                String tileId = "tile_" + r + c;
+                int resId = getResources().getIdentifier(tileId, "id", getPackageName());
+                grid[r][c] = findViewById(resId);
+            }
+        }
+    }
 
-        for (char letter : letters.toCharArray()) {
-            int buttonId = getResources().getIdentifier("btn" + letter, "id", getPackageName());
-            if (buttonId != 0) {
-                Button button = findViewById(buttonId);
-                if (button != null) {
-                    button.setOnClickListener(v -> onLetterClick(letter));
-                }
+    private void initializeKeyboard() {
+        String keys = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        for (int i = 0; i < keys.length(); i++) {
+            String buttonId = "btn" + keys.charAt(i);
+            int resId = getResources().getIdentifier(buttonId, "id", getPackageName());
+            keyboardButtons[i] = findViewById(resId);
+            final int index = i;
+            keyboardButtons[i].setOnClickListener(v -> addLetter(keys.charAt(index)));
+        }
+
+        findViewById(R.id.btnEnter).setOnClickListener(v -> checkWord());
+        findViewById(R.id.btnBackspace).setOnClickListener(v -> removeLetter());
+    }
+
+    private void addLetter(char letter) {
+        if (currentGuess.length() < 5) {
+            int col = currentGuess.length();
+            grid[row][col].setText(String.valueOf(letter));
+            currentGuess.append(letter);
+        }
+    }
+
+    private void removeLetter() {
+        if (currentGuess.length() > 0) {
+            int col = currentGuess.length() - 1;
+            grid[row][col].setText("");
+            currentGuess.deleteCharAt(col);
+        }
+    }
+
+    private void checkWord() {
+        if (currentGuess.length() < 5) {
+            Toast.makeText(this, "Enter a 5-letter word", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String guess = currentGuess.toString();
+        boolean[] correct = new boolean[5];
+
+        for (int i = 0; i < 5; i++) {
+            if (guess.charAt(i) == ANSWER.charAt(i)) {
+                grid[row][i].setBackgroundColor(Color.GREEN);
+                correct[i] = true;
             }
         }
 
-        findViewById(R.id.btnEnter).setOnClickListener(v -> onEnterClick());
-        findViewById(R.id.btnBackspace).setOnClickListener(v -> onBackspaceClick());
-    }
+        for (int i = 0; i < 5; i++) {
+            if (!correct[i] && ANSWER.contains(String.valueOf(guess.charAt(i)))) {
+                grid[row][i].setBackgroundColor(Color.YELLOW);
+            } else if (!correct[i]) {
+                grid[row][i].setBackgroundColor(Color.GRAY);
+                disableButton(guess.charAt(i));
+            }
+        }
 
-    private void onLetterClick(char letter) {
-        if (currentInput.length() < MAX_WORD_LENGTH) {
-            currentInput.append(letter);
-            updateDisplay();
+        if (guess.equals(ANSWER)) {
+            Toast.makeText(this, "You guessed it!", Toast.LENGTH_LONG).show();
+        } else if (row < 5) {
+            row++;
+            currentGuess.setLength(0);
+        } else {
+            Toast.makeText(this, "Game Over!", Toast.LENGTH_LONG).show();
         }
     }
 
-    private void onBackspaceClick() {
-        if (currentInput.length() > 0) {
-            currentInput.deleteCharAt(currentInput.length() - 1);
-            updateDisplay();
+    private void disableButton(char letter) {
+        for (Button button : keyboardButtons) {
+            if (button.getText().toString().equals(String.valueOf(letter))) {
+                button.setEnabled(false);
+                button.setBackgroundColor(Color.DKGRAY);
+                break;
+            }
         }
-    }
-
-    private void onEnterClick() {
-        if (currentInput.length() == MAX_WORD_LENGTH) {
-            // Handle word submission logic
-            validateWord(currentInput.toString());
-            currentInput.setLength(0);
-            updateDisplay();
-        }
-    }
-
-    private void updateDisplay() {
-        inputTextView.setText(currentInput.toString());
-    }
-
-    private void validateWord(String word) {
-        // Implement your Wordle logic here (e.g., checking against a word list)
     }
 }
 
